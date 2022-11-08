34f09d9104e7d9cda8de3120073f9b5bccc5735b
doanhnhantre <hieulechanhkk@gmail.com>
2022-11-08 22:18:53 +0700

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+function App() {
+  let hihi: string = "NguyenDucHieu";
+  console.log(hihi)
+  return (
+    <div className=' mx-auto'>
+    </div>
+  );
+}
+
+export default App;
